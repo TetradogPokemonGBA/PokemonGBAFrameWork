202b199c8e1cb8803a9120594e794f801c8d7a80
tetraadog <tetradog@gmail.com>
2016-09-22 21:44:10 +0200

    desarrollando ataque

--- PokemonGBAFrameWork/FrameWork/Ataque.cs
+++ PokemonGBAFrameWork/FrameWork/Ataque.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PokemonGBAFrameWork
-{
+{//es muy extenso..por acabar de desarrollar (hacer clase para trabajar los efectos cómodamente y las demás partes que lo requieran
    public class Ataque
     {
         const int MAXATAQUESSINASM = 511;//hasta que no sepa como se cambia para poner más se queda este maximo :) //hay un tutorial de como hacerlo pero se necesita insertar una rutina ASM link:http://www.pokecommunity.com/showthread.php?t=263479
