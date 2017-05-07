7db505230a0e6bf07709d09209a68dd4aad85f14
Gabriel <tetradog@gmail.com>
2017-05-07 20:57:46 +0200

    añadido la posibilidad de compilar usando de forma externa

---
+++ PokemonGBAFrameWork/PokemonFrameWork/ASM/ASM.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: pikachu240
+ * Fecha: 07/05/2017
+ * Hora: 20:22
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.IO;
+using Gabriel.Cat.Extension;
+namespace PokemonGBAFrameWork
+{
+	/// <summary>
+	/// Description of ASM.
+	/// </summary>
+	public static class ASM
+	{
+		public static readonly string RutaThumb=Environment.CurrentDirectory+System.IO.Path.AltDirectorySeparatorChar+"thumb.bat";
+		public const string MESNAJEFINCORRECTO="Assembled succcessfully";
+		
+		public static byte[] Compilar(string asmCode)
+		{
+			//llamo al compilador y si no da el mensaje de compilado correctamente lanza una excepcion con el mensaje
+			string pathAsmCode=System.IO.Path.GetTempFileName();
+			string pathAsmCompilado=System.IO.Path.GetTempFileName()+".bin";
+			string mensajeFinProceso;
+			byte[] codigoCompilado;
+			System.Diagnostics.Process proceso;
+			System.IO.File.AppendAllText(pathAsmCode,asmCode);
+		    proceso=System.Diagnostics.Process.Start(RutaThumb,pathAsmCode+" "+pathAsmCompilado);
+		    mensajeFinProceso=  proceso.StandardOutput.ReadLine();
+		    if(File.Exists(pathAsmCode))
+		    		File.Delete(pathAsmCode);
+		    if(mensajeFinProceso!=MESNAJEFINCORRECTO)
+		    {
+		    	throw new ASMCompilerException(mensajeFinProceso);
+		    }
+		    
+		    codigoCompilado=File.ReadAllBytes(pathAsmCompilado);
+		    File.Delete(pathAsmCompilado);
+		    
+		    return codigoCompilado;
+			
+		}
+	}
+	public class ASMCompilerException:Exception
+	{
+		public ASMCompilerException(string error):base(error)
+		{}
+	}
+}
