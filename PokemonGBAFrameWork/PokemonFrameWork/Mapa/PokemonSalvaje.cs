7b7f24cf5bc9b5bc35d17d07cc2e935c97cc2c00
Gabriel <tetradog@gmail.com>
2017-05-29 07:54:21 +0200

    desarrollando parte pokemon salvajes sacado de MEH

---
+++ PokemonGBAFrameWork/PokemonFrameWork/Mapa/PokemonSalvaje.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: pikachu240
+ * Sacado de MEH autor shinyQuagsire
+ * Fecha: 29/05/2017
+ * Hora: 6:43
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using Gabriel.Cat.Extension;
+
+namespace PokemonGBAFrameWork
+{
+	/// <summary>
+	/// Description of PokemonSalvaje.
+	/// </summary>
+	public class PokemonSalvaje
+	{
+		public const int LENGHT=4;
+		Pokemon pokemon;
+		byte nivelMinimo;
+		byte nivelMaximo;
+		
+		public PokemonSalvaje()
+		{
+		}
+		public PokemonSalvaje(Pokemon pokemon,int nivelMinimo,int nivelMaximo)
+		{
+			Pokemon=pokemon;
+			NivelMinimo=(byte)nivelMinimo;
+			NivelMaximo=(byte)nivelMaximo;
+		}
+		
+		//será el orden nacional?
+		public Pokemon Pokemon {
+			get {
+				return pokemon;
+			}
+			set {
+				pokemon = value;
+			}
+		}
+
+		public byte NivelMinimo {
+			get {
+				return nivelMinimo;
+			}
+			set {
+				nivelMinimo = value;
+			}
+		}
+
+		public byte NivelMaximo {
+			get {
+				return nivelMaximo;
+			}
+			set {
+				nivelMaximo = value;
+			}
+		}
+		public static PokemonSalvaje GetPokemonSalvaje(RomGba rom,IList<Pokemon> pokedex,int offsetPokemon)
+		{
+			PokemonSalvaje pokemon=new PokemonSalvaje();
+			Pokemon.OrdenPokemon orden=Pokemon.Orden;
+			Pokemon.Orden=Pokemon.OrdenPokemon.Nacional;//supongo que es este pero no lo se...por mirar...
+			pokedex=pokedex.SortByQuickSort();
+			Pokemon.Orden=orden;
+			pokemon.NivelMinimo=rom.Data[offsetPokemon++];
+			pokemon.NivelMaximo=rom.Data[offsetPokemon++];
+			pokemon.Pokemon=pokedex[Word.GetWord(rom,offsetPokemon)];
+			return pokemon;
+		}
+		public static void SetPokemonSalvaje(RomGba rom,int offsetPokemon,PokemonSalvaje pokemonSalvaje)
+		{
+			rom.Data[offsetPokemon++]=pokemonSalvaje.NivelMinimo;
+			rom.Data[offsetPokemon++]=pokemonSalvaje.NivelMaximo;
+			Word.SetWord(rom,offsetPokemon,(short)pokemonSalvaje.Pokemon.OrdenNacional);
+		}
+	}
+}
