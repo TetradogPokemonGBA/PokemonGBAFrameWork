0c92bec96c37e731b1916b231a06b4a6b22efd5b
Gabriel <tetradog@gmail.com>
2017-08-16 00:58:19 +0200

    de momento lo dejo asi, más adelante añadiré el codigo fuente de todas las ediciones y también la posibiliadad de cambiar de variable :)

--- PokemonGBAFrameWork/PokemonFrameWork/Parches/Shinyzer.cs
+++ PokemonGBAFrameWork/PokemonFrameWork/Parches/Shinyzer.cs
@@ -5,7 +5,7 @@
  * Time: 10:53
  * 
  * Código bajo licencia GNU
- *
+ * creditos a hackmew por la rutina <3
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
@@ -20,9 +20,38 @@ namespace PokemonGBAFrameWork
 {
 	public static class Shinyzer
 	{
-		public static readonly ASM RutinaEsmeralda=ASM.Compilar(Resources.ASMShinyzer);
-		public static int VariableShinytzer = (int)(Hex)"8003";//mirar de poder cambiarla...
+		//public static readonly ASM RutinaEsmeralda=ASM.Compilar(Resources.ASMShinyzer);
+		public static readonly int VariableShinytzer = (int)(Hex)"8003";//mirar de poder cambiarla...hasta que no lo sepa hacer será readonly
+		#region Rutina
+		//mas adelante poner el codigo fuente y usar la clase ASM
+		public static readonly byte[] RutinaComun = {
+			0x20, 0x0E, 0x03, 0x28, 0x03, 0xD1, 0x20, 0x48, 0x00, 0x78, 0x00, 0x28,
+			0x00, 0xD1, 0x70, 0x47, 0x3C, 0xB5, 0x43, 0x1E, 0x1C, 0x48, 0x03, 0x70,
+			0x44, 0x78, 0x00, 0x2C, 0x26, 0xD1, 0x0C, 0x1C, 0x1A, 0x4A, 0x00, 0xF0,
+			0x2A, 0xF8, 0x07, 0x23, 0x18, 0x40, 0x03, 0x1C, 0x17, 0x4A, 0x00, 0xF0,
+			0x24, 0xF8, 0x05, 0x04, 0x05, 0x43, 0x5D, 0x40, 0x65, 0x40, 0x70, 0xB4,
+			0x29, 0x0C, 0x28, 0x04, 0xDB, 0x43, 0x1B, 0x0C, 0x0D, 0x4C, 0x0E, 0x4D,
+			0x06, 0x1C, 0x66, 0x43, 0x76, 0x19, 0x32, 0x0C, 0x8A, 0x42, 0x04, 0xD0,
+			0x01, 0x30, 0x01, 0x3B, 0x00, 0x2B, 0xF5, 0xD1, 0x04, 0xE0, 0x09, 0x4A,
+			0x16, 0x60, 0x62, 0xBC, 0x3D, 0x60, 0x3C, 0xBD, 0x70, 0xBC, 0xD9, 0xE7,
+			0x01, 0x25, 0x9D, 0x40, 0x2C, 0x40, 0x00, 0x2C, 0x00, 0xD0, 0x39, 0x68,
+			0xF5, 0xE7, 0x10, 0x47, 0x6D, 0x4E, 0xC6, 0x41, 0x73, 0x60, 0x00, 0x00}
+		;
+		public static readonly byte[] RutinaEsmeralda = {
+			0x80, 0x5D, 0x00, 0x03, 0xDE, 0x75, 0x03, 0x02, 0xCD, 0xF5, 0x06, 0x08
+		};
 
+
+		public static readonly byte[] RutinaRojoYVerde = {
+			0x00, 0x50, 0x00, 0x03, 0xBE, 0x70, 0x03, 0x02, 0xC9, 0x4E, 0x04, 0x08
+		};
+
+		public static readonly byte[] RutinaRubiYZafiro = {
+			0x18, 0x48, 0x00, 0x03, 0xCA, 0xE8, 0x02, 0x02, 0x85, 0x0E, 0x04, 0x08
+		};
+
+
+		#endregion
 		const byte BYTE1ON=0x88;
 		const byte BYTE2ON=0x88;
 		const byte BYTE3ON=0x4;
@@ -43,7 +72,6 @@ namespace PokemonGBAFrameWork
 		const int SUMAENTREBYTES=2;
 		const int SUMAENTREARRAYS=3;
 		
-		//falta la rutina de cada uno
 		
 		static Shinyzer()
 		{
@@ -65,29 +93,29 @@ namespace PokemonGBAFrameWork
 			
 			//se tiene que mirar el codigo fuente...para la variable poderla cambiar
 			//falta añadir el codigo fuente de todos...de momento pondré los bytes compilados...mas adelante poner el codigo fuente :)
-			//aux=COmpilo RUBI y SHAPHIRE//los ultimos 4 bytes es un pointer a un texto
+			aux=RutinaRubiYZafiro;//Compilo RUBI y SHAPHIRE//los ultimos 4 bytes es un pointer a un texto
 			DicRutina.Add(EdicionPokemon.RubiUsa,aux);
 			DicRutina.Add(EdicionPokemon.ZafiroUsa,aux);
 			DicRutina.Add(EdicionPokemon.RubiEsp,aux);
 			DicRutina.Add(EdicionPokemon.ZafiroEsp,aux);
-			//aux=Compilo Verde y Rojo
+			aux=RutinaRojoYVerde;//Compilo Verde y Rojo
 			DicRutina.Add(EdicionPokemon.VerdeHojaUsa,aux);
 			DicRutina.Add(EdicionPokemon.RojoFuegoUsa,aux);
 			DicRutina.Add(EdicionPokemon.VerdeHojaEsp,aux);
 			DicRutina.Add(EdicionPokemon.RojoFuegoEsp,aux);
 			
-			aux=RutinaEsmeralda.AsmBinary;
+			aux=RutinaEsmeralda;//RutinaEsmeralda.AsmBinary;
 			DicRutina.Add(EdicionPokemon.EsmeraldaUsa,aux);
 			DicRutina.Add(EdicionPokemon.EsmeraldaEsp,aux);
 			
 			
 
 		}
-		public static bool EstaActivado(RomGba rom,EdicionPokemon edicion,Compilacion compilacion)
+		public static bool EstaActivado(RomGba rom)
 		{
-			if (rom == null||edicion==null||compilacion==null)
+			if (rom == null)
 				throw new ArgumentNullException();
-			return rom.Data.SearchArray(Variable.GetVariable(VariableEdicionArray2,edicion,compilacion),Array2OFF)>0;
+			return PosicionRutinaShinyzer(rom)>0;
 		}
 		public static int Activar(RomData rom)
 		{
@@ -95,9 +123,11 @@ namespace PokemonGBAFrameWork
 		}
 		public static int Activar(RomGba rom,EdicionPokemon edicion,Compilacion compilacion)
 		{
-			int posicion = PosicionRutinaShinyzer(rom,edicion);
+			int posicion = PosicionRutinaShinyzer(rom);
 			int offsetAct;
 			if(posicion<0){
+				//más adelante añadir la variable :) asi se puede personalizar :D
+				//el pointer que apunta a un texto también pero cuando sepa que es :)
 				offsetAct=Variable.GetVariable(VariablePosicionInicio,edicion,compilacion);
 				rom.Data[offsetAct]=BYTE1ON;
 				offsetAct+=SUMAENTREBYTES;
@@ -109,8 +139,11 @@ namespace PokemonGBAFrameWork
 				offsetAct+=SUMAENTREARRAYS;
 				rom.Data.SetArray(offsetAct,Array2ON);
 				offsetAct+=Array2ON.Length;
-				posicion=rom.Data.SetArray(DicRutina[edicion]);
-				rom.Data.SetArray(offsetAct,new PokemonGBAFrameWork.OffsetRom(posicion).BytesPointer);//pongo el pointer a al rutina
+				
+				posicion=rom.Data.SearchEmptyBytes(RutinaComun.Length+DicRutina[edicion].Length);
+				rom.Data.SetArray(posicion,RutinaComun);
+				rom.Data.SetArray(posicion+RutinaComun.Length,DicRutina[edicion]);//los ultimos 4 bytes son para un pointer que va aun texto...de momento dejo el que hay por defecto :)
+				rom.Data.SetArray(offsetAct,new PokemonGBAFrameWork.OffsetRom(posicion+1).BytesPointer);//pongo el pointer a al rutina+1 porque asi es como se ponen los offsets de las rutinas :)
 				
 			}
 			return posicion;
@@ -118,11 +151,11 @@ namespace PokemonGBAFrameWork
 		}
 		public static void Desactivar(RomGba rom,EdicionPokemon edicion,Compilacion compilacion)
 		{
-			int posicion = PosicionRutinaShinyzer(rom,edicion);
+			int posicion = PosicionRutinaShinyzer(rom);
 			int offsetAct;
 			if (posicion > 0)
 			{
-				rom.Data.Remove( posicion, DicRutina[edicion].Length);
+				rom.Data.Remove(posicion,RutinaComun.Length+DicRutina[edicion].Length);
 				offsetAct=Variable.GetVariable(VariablePosicionInicio,edicion,compilacion);
 				rom.Data[offsetAct]=BYTE1OFF;
 				offsetAct+=SUMAENTREBYTES;
@@ -141,11 +174,11 @@ namespace PokemonGBAFrameWork
 		/// </summary>
 		/// <param name="rom"></param>
 		/// <returns>devuelve -1 si no lo ha encontrado</returns>
-		public  static int PosicionRutinaShinyzer(RomGba rom,EdicionPokemon edicion)
+		public  static int PosicionRutinaShinyzer(RomGba rom)
 		{
-			if (rom == null||edicion==null)
+			if (rom == null)
 				throw new ArgumentNullException();
-			return rom.Data.SearchArray(DicRutina[edicion]);
+			return rom.Data.SearchArray(RutinaComun);//mirar si esta en la parte común la variable
 		}
 		public static ComandosScript.SetVar ScriptLineaPokemonShiny(short numPokemon)
 		{
