fd44f4d6887f32f685b0113dd4a2f06798a9808f
Gabriel <tetradog@gmail.com>
2017-10-06 17:58:10 +0200

    añadiendo informacion

---
+++ PokemonGBAFrameWork/PokemonFrameWork/Parches/ColorearStats.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Pikachu240
+ * Fecha: 03/10/2017
+ * Hora: 4:57
+ * Licencia GNU GPL V3
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace PokemonGBAFrameWork.PokemonFrameWork.Parches
+{
+	/// <summary>
+	/// Description of ColorearStats.
+	/// </summary>
+	public class ColorearStats
+	{
+		public ColorearStats()
+		{
+		}
+	}
+}
--- PokemonGBAFrameWork/Properties/AssemblyInfo.cs
+++ PokemonGBAFrameWork/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle ("PokemonGBAFrameWork")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("github.com/TetradogPokemonGBA/PokemonGBAFrameWork/")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("PokemonGBAFrameWork")]
