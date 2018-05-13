1c158a95c63ccb144b5bb1e30421d02e5bb82738
Gabriel <tetradog@gmail.com>
2018-05-13 20:21:00 +0200

    desarrollando :)

--- PokemonGBAFrameWork/GBACore/Parche.cs
+++ PokemonGBAFrameWork/GBACore/Parche.cs
@@ -26,7 +26,7 @@ namespace PokemonGBAFrameWork
                 byte[] dataOn = (byte[])BytesOn.Clone();
                 for (int i = 0; i < PointersRelativos.Count; i++)
                 {
-                    OffsetRom.SetOffset(dataOn, PointersRelativos[i].Key,new OffsetRom(rom.Data.SetArrayIfNotExist(PointersRelativos[i].Value.Datos.AddArray(space))));
+                    OffsetRom.SetOffset(dataOn, PointersRelativos[i].Key,new OffsetRom(rom.Data.SetArrayIfNotExist(PointersRelativos[i].Value.GetBytes(rom))));
                 }
                 rom.Data.SetArray(Offset.Offset, dataOn);
             }
@@ -36,6 +36,25 @@ namespace PokemonGBAFrameWork
         {
 
             public byte[] Datos { get; set; }
+            public Llista<KeyValuePair<OffsetRom, ParteRelativa>> PartesRelativas { get; private set; }
+
+            public ParteRelativa()
+            {
+                PartesRelativas = new Llista<KeyValuePair<OffsetRom, ParteRelativa>>();
+            }
+            public byte[] GetBytes(RomGba rom)
+            {
+                byte[] space = new byte[5];//lo añado por si existe una rutina igual pero un poco más larga que no coja esa rutina como la rutina que buscamos.
+
+                byte[] datos =PartesRelativas.Count>0? (byte[]) Datos.Clone():Datos;
+                for(int i=0;i<PartesRelativas.Count;i++)
+                {
+                    OffsetRom.SetOffset(datos, PartesRelativas[i].Key, new OffsetRom(rom.Data.SetArrayIfNotExist(PartesRelativas[i].Value.Datos.AddArray(space))));
+
+                }
+
+                return datos;
+            }
         }
 
         public string GameCode { get; set; }
@@ -50,9 +69,12 @@ namespace PokemonGBAFrameWork
         }
         public Parche(RomGba romBase,RomGba romDataPatch,bool forcePatch=false)
         {
+            const int SEPARACIONMIN = OffsetRom.LENGTH;
+
             if (!forcePatch&&romBase.Edicion.GameCode != romDataPatch.Edicion.GameCode)
                 throw new RomNoCompatibleException(romBase.Edicion.GameCode, romDataPatch.Edicion.GameCode);
             //si los datos del parche sobreescriben datos de la rombase son partesAbsolutas si sobrescriben partes vacias son partes relativas
+            //no empieza otra parte si la distancia no supera el minimo
 
         }
         public void SetPatch(RomGba rom,bool forcePatch=false)
@@ -61,11 +83,8 @@ namespace PokemonGBAFrameWork
             if (!forcePatch && rom.Edicion.GameCode != GameCode)
                 throw new RomNoCompatibleException(GameCode);
 
-           
-
             for (int i = 0; i < PartesAbsolutas.Count; i++)
                 PartesAbsolutas[i].SetData(rom);
-
 
         }
 
