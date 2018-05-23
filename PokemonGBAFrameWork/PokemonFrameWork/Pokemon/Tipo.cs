4089541bb36bc261f2444300658eedf0369f3adb
Gabriel <tetradog@gmail.com>
2018-05-23 19:57:07 +0200

    subir clase

--- PokemonGBAFrameWork/PokemonFrameWork/Pokemon/Tipo.cs
+++ PokemonGBAFrameWork/PokemonFrameWork/Pokemon/Tipo.cs
@@ -1,4 +1,5 @@
 ﻿using Gabriel.Cat;
+using PokemonGBAFrameWork.Tipo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,31 +9,20 @@ using System.Threading.Tasks;
 namespace PokemonGBAFrameWork
 {
 	
-	public  class Tipo
+	public  class TipoCompleto
 	{
-		public enum LongitudCampo
-		{ Nombre=7 }
-		public static readonly Zona ZonaNombreTipo;
+	
+	
 		public static readonly Zona ZonaImagenTipo;
-		static Tipo()
+
+        Nombre nombre;
+
+		static TipoCompleto()
 		{
-			ZonaNombreTipo = new Zona("Nombre Tipo");
+			
 			ZonaImagenTipo = new Zona("Imagen Tipo");
 			
-			ZonaNombreTipo.Add(EdicionPokemon.ZafiroEsp, 0x2E574);
-			ZonaNombreTipo.Add(EdicionPokemon.RubiEsp, 0x2E574);
 
-			ZonaNombreTipo.Add(EdicionPokemon.ZafiroUsa, 0x2E3A8);
-			ZonaNombreTipo.Add(EdicionPokemon.RubiUsa, 0x2E3A8);
-
-			ZonaNombreTipo.Add(EdicionPokemon.EsmeraldaEsp, 0x166F4);
-			ZonaNombreTipo.Add(EdicionPokemon.EsmeraldaUsa, 0x166F4);
-
-			ZonaNombreTipo.Add(EdicionPokemon.RojoFuegoEsp, 0x308B4);
-			ZonaNombreTipo.Add(EdicionPokemon.VerdeHojaEsp, 0x308B4);
-
-			ZonaNombreTipo.Add(EdicionPokemon.RojoFuegoUsa, 0x309C8, 0x309DC);
-			ZonaNombreTipo.Add(EdicionPokemon.VerdeHojaUsa, 0x309C8, 0x309DC);
 
 			//añado las imagenes son algo pero no se que son...
 			ZonaImagenTipo.Add(EdicionPokemon.VerdeHojaUsa, 0x107d88, 0x107e00);
@@ -45,38 +35,22 @@ namespace PokemonGBAFrameWork
 			
 
 		}
-		BloqueString nombre;
+		
 
 
-		public Tipo(BloqueString nombre)
-		{
-			if (nombre == null) throw new ArgumentNullException();
-			this.nombre = nombre;
-			Nombre.MaxCaracteres = (int)LongitudCampo.Nombre;
-		}
 
-		public BloqueString Nombre
-		{
-			get
-			{
-				return nombre;
-			}
-
-			private set
-			{
-				nombre = value;
-			}
-		}
 		public override string ToString()
 		{
-			return Nombre;
+			return nombre.Texto;
 		}
-        public static Tipo GetTipo(RomGba rom,int posicion)
+        public static TipoCompleto GetTipo(RomGba rom,int posicion)
         {
 
 			if (rom == null ||  posicion < 0) throw new ArgumentException();
-			BloqueString blNombre = BloqueString.GetString(rom, Zona.GetOffsetRom(ZonaNombreTipo, rom,  rom.Edicion).Offset + posicion * (int)LongitudCampo.Nombre, (int)LongitudCampo.Nombre,true);
-			return new Tipo(blNombre);
+
+            TipoCompleto tipo = new TipoCompleto();
+            tipo.nombre = Nombre.GetNombre(rom, posicion);
+            return tipo;
 		}
 		public static int GetTotal(RomGba rom)
 		{
@@ -84,36 +58,32 @@ namespace PokemonGBAFrameWork
 			return 18;
 		}
 
-		public static Tipo[] GetTipos(RomGba rom)
+		public static TipoCompleto[] GetTipos(RomGba rom)
 		{
-			Tipo[] tipos = new Tipo[GetTotal(rom)];
+            TipoCompleto[] tipos = new TipoCompleto[GetTotal(rom)];
 			for (int i = 0; i < tipos.Length;i++)
 				tipos[i] = GetTipo(rom, i);
 			return tipos;
 		}
 
-		public static void SetTipo(RomGba rom, Tipo tipo, int posicion)
+		public static void SetTipo(RomGba rom, TipoCompleto tipo, int posicion)
 		{
-			if (rom == null ||tipo == null || tipo.Nombre.Texto.Length > (int)LongitudCampo.Nombre || posicion < 0) throw new ArgumentException();
-			int offset = Zona.GetOffsetRom(ZonaNombreTipo, rom, rom.Edicion).Offset + posicion * (int)LongitudCampo.Nombre;
-			
-			BloqueString.Remove(rom,offset);
-			BloqueString.SetString(rom, offset, tipo.Nombre);
+			if (rom == null ||tipo == null || posicion < 0) throw new ArgumentException();
+            Nombre.SetNombre(rom, posicion, tipo.nombre);
 
 		}
 
-		public static void SetTipos(RomGba rom,  IList<Tipo> tipos)
+		public static void SetTipos(RomGba rom,  IList<TipoCompleto> tipos)
 		{
-			OffsetRom offsetNombres;
+
 			if (rom == null || tipos == null) throw new ArgumentNullException();
-			if (tipos.Count != GetTotal(rom))
-			{
-				offsetNombres=Zona.GetOffsetRom(ZonaNombreTipo,rom,rom.Edicion);
-				rom.Data.Remove(offsetNombres.Offset, GetTotal(rom) * (int)LongitudCampo.Nombre);
-				OffsetRom.SetOffset(rom, offsetNombres, rom.Data.SearchEmptyBytes(tipos.Count * (int)LongitudCampo.Nombre));//actualizo el offset
-			}
-			for (int i = 0; i < tipos.Count; i++)
-				SetTipo(rom, tipos[i], i);
+
+            List<Nombre> nombres = new List<Nombre>();
+            for (int i = 0; i < tipos.Count; i++)
+            {
+                nombres.Add(tipos[i].nombre);
+            }
+            Nombre.SetNombre(rom, nombres);
 		}
 	}
 }
---
+++ PokemonGBAFrameWork/PokemonFrameWork/Pokemon/TipoNombre.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonGBAFrameWork.Tipo
+{
+    public class Nombre
+    {
+        public enum LongitudCampo
+        { Nombre = 7 }
+        public static readonly Zona ZonaNombreTipo;
+
+        public BloqueString Texto { get; set; }
+        static Nombre()
+        {
+            ZonaNombreTipo = new Zona("Nombre Tipo");
+            ZonaNombreTipo.Add(EdicionPokemon.ZafiroEsp, 0x2E574);
+            ZonaNombreTipo.Add(EdicionPokemon.RubiEsp, 0x2E574);
+
+            ZonaNombreTipo.Add(EdicionPokemon.ZafiroUsa, 0x2E3A8);
+            ZonaNombreTipo.Add(EdicionPokemon.RubiUsa, 0x2E3A8);
+
+            ZonaNombreTipo.Add(EdicionPokemon.EsmeraldaEsp, 0x166F4);
+            ZonaNombreTipo.Add(EdicionPokemon.EsmeraldaUsa, 0x166F4);
+
+            ZonaNombreTipo.Add(EdicionPokemon.RojoFuegoEsp, 0x308B4);
+            ZonaNombreTipo.Add(EdicionPokemon.VerdeHojaEsp, 0x308B4);
+
+            ZonaNombreTipo.Add(EdicionPokemon.RojoFuegoUsa, 0x309C8, 0x309DC);
+            ZonaNombreTipo.Add(EdicionPokemon.VerdeHojaUsa, 0x309C8, 0x309DC);
+        }
+        public Nombre(string nombre="")
+        {
+            Texto = new BloqueString((int)LongitudCampo.Nombre);
+            Texto.Texto = nombre;
+        }
+        public static Nombre GetNombre(RomGba rom,int index)
+        {
+            Nombre nombre=new Nombre();
+            nombre.Texto.Texto= BloqueString.GetString(rom, Zona.GetOffsetRom(ZonaNombreTipo, rom, rom.Edicion).Offset + index * (int)LongitudCampo.Nombre, (int)LongitudCampo.Nombre, true).Texto;
+            return nombre;
+        }
+        public static void SetNombre(RomGba rom,int index,Nombre nombre)
+        {
+            BloqueString.SetString(rom, Zona.GetOffsetRom(ZonaNombreTipo, rom, rom.Edicion).Offset + index * (int)LongitudCampo.Nombre, nombre.Texto);
+        }
+        public static void SetNombre(RomGba rom,IList<Nombre> nombres)
+        {
+            int total = TipoCompleto.GetTotal(rom);
+            rom.Data.Remove(Zona.GetOffsetRom(ZonaNombreTipo, rom, rom.Edicion).Offset, total * (int)LongitudCampo.Nombre);
+            OffsetRom.SetOffset(rom, Zona.GetOffsetRom(ZonaNombreTipo, rom, rom.Edicion), rom.Data.SearchEmptyBytes(nombres.Count * (int)LongitudCampo.Nombre));
+            for (int i = 0; i < nombres.Count; i++)
+                SetNombre(rom, i, nombres[i]);
+        }
+    }
+}
